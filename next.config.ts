533ac8f4a401e305686d8f9ef0d0f9b620d67e48
Alex Deonise <83414814+Maxxtra@users.noreply.github.com>
2026-05-27 14:17:44 +0300

    Fix formatting and update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,22 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
+
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: isProd ? "/site" : "",
+  assetPrefix: isProd ? "/site/" : "",
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'bcalabs.org',
+        protocol: "https",
+        hostname: "bcalabs.org",
       },
     ],
   },
